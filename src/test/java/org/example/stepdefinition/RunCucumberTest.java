a85b25bc4e27e8b903b369104df5ff414d1f9b4d
jagandeep <nice.jagan@gmail.com>
2021-01-03 11:54:54 +0530

    empty project for cucmber

---
+++ src/test/java/org/example/stepdefinition/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package org.example.stepdefinition;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"},
+        features = { "classpath:features/test.feature" })
+public class RunCucumberTest {
+}
